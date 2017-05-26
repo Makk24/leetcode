58b00ee2401fc38af23b99ab022518902ae1fe58
1009576562@qq.com <1009576562@qq.com>
2017-05-26 16:33:57 +0800

    0526

---
+++ ZigZag Conversion.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        l=len(s)
+        n=numRows
+        str=''
+        if l>2 and n>1:
+            m=l//(n-1)
+            a=l%(n-1)
+            if a==0:
+                
+            for x in range(l):
+                if n>=x:
+                    
